5fddee81b2012d28fc71ff53ed7474ecc5b71f84
bloriot97 <bloriot97@gmail.com>
2019-06-17 08:57:59 +0200

    test update

--- startled.py
+++ startled.py
@@ -4,6 +4,8 @@ import time
 ledCircle = LedCircle.LedCircle()
 ledCircle.start()
 ledCircle.animation = "fade"
-ledCircle.duration = 1
-time.sleep(1)
+ledCircle.duration = 0.5
+time.sleep(0.5)
+ledCircle.duration = 0.5
+time.sleep(0.5)
 ledCircle.stop()
